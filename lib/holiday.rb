a21835782f3e5839dfcafcc11f7e2197e925b3c4
Vince W <vincewallgren@me.com>
2021-03-16 04:32:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  holiday_hash[:summer][fourth_of_july][1]
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
